d3b232c3e69a395972661d913d19f40b4ce444ae
MelchorRuiz <me@melchor-ruiz.fr>
2025-02-09 11:12:50 +0100

    refactor: remove AppService and AppController, add database configuration for TypeORM

--- apps/realtime-elo-ranker-server/src/app.module.ts
+++ apps/realtime-elo-ranker-server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './config/database.config';
+import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    PlayersModule
+  ],
 })
 export class AppModule {}
